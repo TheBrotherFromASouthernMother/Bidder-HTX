683c63fd95445bfa665ad5664d8991017684d4e3
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-04-18 10:05:12 -0500

    added different artworks and made email and password required, unique

--- app/db/sql/artworks/test.sql
+++ app/db/sql/artworks/test.sql
@@ -1,3 +1,5 @@
+/* TEST ENTRY */
+
 INSERT INTO artworks VALUES (
     DEFAULT, 'Test Artwork', 2018, '12" x 12"', 'Acrylic on Canvas', '1st Edition', 'Your Mom', 'This artwork is cool.', 40000.00
 );
@@ -5,3 +7,22 @@ INSERT INTO artworks VALUES (
 INSERT INTO artworks VALUES (
     DEFAULT, 'Test Artwork 2', 2014, '1" x 2"', 'Oil on Canvas', '3rd Edition', 'My Mom', 'This artwork is really cool.', 4.00
 );
+
+/* UPDATE TEST ENTRY */
+
+UPDATE artworks
+SET title = 'Mona Lisa', year = 1506, dimensions = '77 cm × 53 cm (30 in × 21 in)', 
+    materials = 'Oil on poplar panel', edition = '1st Edition', artist = 'Leonardo da Vinci', 
+    detail_desc = 'The <i>Mona Lisa</i> is a half-length portrait painting by the Italian Renaissance artist Leonardo da Vinci. The painting is thought to be a portrait of Lisa Gherardini, the wife of Francesco del Giocondo, and is in oil on a white Lombardy poplar panel. It had been believed to have been painted between 1503 and 1506; however, Leonardo may have continued working on it as late as 1517.', 
+    fmv = 800000000.00,
+    images = array_append(images, 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/687px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg')
+WHERE id = 1;
+
+
+UPDATE artworks
+SET title = 'Girl with a Pearl Earring', year = 1665, dimensions = '44.5 cm × 39 cm (17.5 in × 15 in)', 
+    materials = 'Oil on canvas', edition = '1st Edition', artist = 'Johannes Vermeer', 
+    detail_desc = '<i>Girl with a Pearl Earring</i> is an oil painting by Dutch Golden Age painter Johannes Vermeer. It is a tronie of a girl wearing a headscarf and a pearl earring.',
+    fmv = 40000000.00,
+    images = array_append(images, 'https://upload.wikimedia.org/wikipedia/commons/6/66/Johannes_Vermeer_%281632-1675%29_-_The_Girl_With_The_Pearl_Earring_%281665%29.jpg')
+WHERE id = 2;
--- app/db/sql/payments/test.sql
+++ app/db/sql/payments/test.sql
@@ -1,3 +1,3 @@
 INSERT INTO payments (id, amount, payment_timestamp, user_id, bid_id) VALUES
-    (DEFAULT, 1000.00, '2018-06-10 12:00:00', 2, 1),
-    (DEFAULT, 10000.00, '2018-08-10 12:00:00', 3, 2);
+    (DEFAULT, 1000.00, '2018-06-10 12:00:00', 2, 10),
+    (DEFAULT, 10000.00, '2018-08-10 12:00:00', 3, 12);
