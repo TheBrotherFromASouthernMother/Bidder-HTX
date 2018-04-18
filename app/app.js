371ef6408e80dfd5024a664e05cfff558627698b
Christian Lowe <christglowe@gmail.com>
2018-04-18 10:52:56 -0500

    Created full user authenciation

--- app/app.js
+++ app/app.js
@@ -15,9 +15,9 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const http = require('http');
 const server = http.createServer(app);
-// const io = require('socket.io').listen(server);
-// server.listen(8000);
-// end socket.io requires
+const io = require('socket.io').listen(server);
+server.listen(8000);
+end socket.io requires
 
 const port = process.env.PORT || 3000;
 
@@ -32,43 +32,37 @@ app.use(session({ secret: "cats",
                   }
  }));
 
-/*  connecting heroku db in node.js:
-    install the pg npm module as a dependency then
-    connect to process.env.DATABASE_URL when your app initializes */
 
-    const { Client } = require('pg');
-    const db_url = 'postgres://qgauodbyzimumj:4071b5334860231f7881bb907f5fbc9e9fad3e60b9c759cd8bdac0214daf670d@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d5df5c8pc330kr';
+const promise = require('bluebird');
 
-    const client = new Client({
-      connectionString: process.env.db_url,
-      ssl: true,
-    });
+const initOptions = {
+  // Initialization Options
+  promiseLib: promise
+};
 
-client.connect();
+const pgp = require('pg-promise')(initOptions);
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
+
+const db = pgp({
+  connectionString: 'postgres://qgauodbyzimumj:4071b5334860231f7881bb907f5fbc9e9fad3e60b9c759cd8bdac0214daf670d@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d5df5c8pc330kr',
+  ssl: true
 });
-/* end db connection */
 
+module.exports.db = db;
 
-// // socket.io server listening and broadcasting for app.js
-// io.sockets.on('connection', function(socket) {
-//
-//   // listen to incoming bids
-//   socket.on('bid', function(content) {
-//     console.log('bid is: ' + content); // submitted bid is transmitted back to server
-//     // echo to the sender
-//     socket.emit('bid', content['amount']);
-//
-//     // broadcast the bid to all clients
-//     socket.broadcast.emit('bid', socket.id + 'bid: ' + content['amount']);
-//   });
-// });
+// socket.io server listening and broadcasting for app.js
+io.sockets.on('connection', function(socket) {
+
+  // listen to incoming bids
+  socket.on('bid', function(content) {
+    console.log('bid is: ' + content); // submitted bid is transmitted back to server
+    // echo to the sender
+    socket.emit('bid', content['amount']);
+
+    // broadcast the bid to all clients
+    socket.broadcast.emit('bid', socket.id + 'bid: ' + content['amount']);
+  });
+});
 
 
 
@@ -77,9 +71,10 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(require("./routes/login-route.js"));
-// app.use(require("./routes/register.js"));
-// app.use(require("./routes/payment-route.js"));
+app.use(require("./routes/register.js"));
+app.use(require("./routes/payment-route.js"));
 app.use(require("./routes/artwork-route.js"));
+app.use(require('./routes/logout-route.js'));
 
 app.get('/', (req, res, next) => {
   res.send("site under construction");
--- app/routes/artwork-route.js
+++ app/routes/artwork-route.js
@@ -3,15 +3,8 @@ var promise = require('bluebird');
 const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended :false}))
 
-const initOptions = {
-    // Initialization Options
-    promiseLib: promise
-  };
-  const pgp = require('pg-promise')(initOptions);
-  const db = pgp({
-    connectionString: 'postgres://qgauodbyzimumj:4071b5334860231f7881bb907f5fbc9e9fad3e60b9c759cd8bdac0214daf670d@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d5df5c8pc330kr',
-    ssl: true
-  });
+const db = require("../app.js").db;
+
 
 const authenticateUser = require('./authenticateUser.js').authenticateUser;
 
