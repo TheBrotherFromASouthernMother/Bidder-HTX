059ad9a1de3c8fce2171571c8f997991132dd2bb
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-04-18 14:52:09 -0500

    adding post stuff

--- app/public/js/bid-nav.js
+++ app/public/js/bid-nav.js
@@ -1,8 +1,11 @@
 var bidButtons = $(".bid-on-it");
 
-bidButtons.forEach( e => {
-    e.click( () => {
+bidButtons.forEach( element => {
+    element.submit( (event) => {
         console.log("You clicked my button");
         console.log($(this).attr("artwork-id"));
+        $.post("/lot", {
+            artworkId: $(this).attr("artwork-id")
+        });
     });
 });
