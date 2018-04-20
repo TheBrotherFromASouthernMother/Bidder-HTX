423c6754b97b3f65e4e898c7dd10d12d97d03bc8
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-04-20 10:54:06 -0500

    updating socket bid function

--- app/public/js/socketIOBid.js
+++ app/public/js/socketIOBid.js
@@ -1,11 +1,17 @@
-
 $(function () {
   var socket = io.connect('http://localhost:8000');
-  
-  $('.submit-bid').submit(function() {
-    let bidEvent = { bidAmount: $('#bid-amount').val(), email: $(this).attr("email"), lotNumber: $(this).attr("lot-number") };
-    socket.emit('bid', bidEvent ); // $('#bid').val() is the value picked up by socket to the server
+  $('.submit-bid').submit( () => {
+    var bidSubmitted = $('#bid-amount').val();
+    console.log(bidSubmitted);
+    var bidEvent = { bidAmount: bidSubmitted, userId: $('.navbar-text').attr("userId"), lotNumber: $('.lot-number').attr("lot-number") };
+    socket.emit('bid', bidEvent ); // bidEvent is the object sent by socket to the server
     console.log(bidEvent);
-    return false; 
+    return false;
+  });
+    // $.ajax( {url: '/before-payment', method: 'GET'} ).success( () => { window.location.assign('/payments') } );
+  $('.submit-bid').submit( () => {
+    setTimeout( () => {
+      window.location.assign('/payment');
+    }, 3000);
   });
 });
