957bfd9a588be2b8757c1c134f49650529469fb0
Christian Lowe <christglowe@gmail.com>
2018-04-20 21:01:30 -0500

    testing server deploy without socket.io

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ const initOptions = {
 
 const pgp = require('pg-promise')(initOptions);
 
+
 const db = pgp({
   connectionString: 'postgres://qgauodbyzimumj:4071b5334860231f7881bb907f5fbc9e9fad3e60b9c759cd8bdac0214daf670d@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d5df5c8pc330kr',
   ssl: true
@@ -27,11 +28,11 @@ handlebars.registerHelper("add", function(num1, num2) {
 const exphbs = require('express-handlebars');
 
 // socket.io requires
-const path = require('path');
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io').listen(server);
-server.listen(8000);
+// const path = require('path');
+// const http = require('http');
+// const server = http.createServer(app);
+// const io = require('socket.io').listen(server);
+// server.listen(8000);
 // end socket.io requires
 
 const port = process.env.PORT || 3000;
@@ -42,41 +43,41 @@ app.use(session({ secret: "cats",
                   resave: true,
                   saveUninitialized: false,
                   cookie: {
-                    expires: true, //closing the browser will not log the user out
+                    expires: false, //closing the browser will not log the user out
                     maxAge: 1 * 24 * 60 * 60 * 1000 //one day
                   }
  }));
 
 // socket.io server
-var clients = 0;
-var bidData;
-// when some client connects:
-io.on('connection', function(socket) {
-  clients++; // client counter decreases
-  console.log('A client has connected. ' + clients + ' now connected.');
-
-  // listen to incoming bids
-  var incomingBid = socket.on('bid', function(bidEvent) {
-    console.log('Incoming bid: ' + bidEvent.bidAmount + ' from ' + bidEvent.userId + ' for lot ' + bidEvent.lotNumber); // submitted bid is transmitted back to server; use content to parse for bidValue, email, lotId to update db (pass to another route js???)
-    var bidData = bidEvent;
-    console.log('updating bids TABLE with: ' + bidData);
-
-    // SQL statement to update bid TABLE
-    db.one('INSERT INTO bids VALUES (DEFAULT, $1, NOW(), FALSE, $2, $3) RETURNING *', [bidEvent.bidAmount, bidEvent.userId, bidEvent.lotNumber])
-      .then(bidData => {
-        console.log('bids TABLE was updated successfully as id#', bidData.id); // confirm bid data updated to db
-      })
-      .catch(error => {
-        console.log('bids TABLE was not updated successfully! ', error);
-      });
-
-  //when some client disconnects:
-    socket.on('disconnect', function() {
-      clients--; // client counter decreases
-      console.log('A client has disconnected. ' + clients + ' still connected.');
-    });
-  });
-});
+// var clients = 0;
+// var bidData;
+// // when some client connects:
+// io.on('connection', function(socket) {
+//   clients++; // client counter decreases
+//   console.log('A client has connected. ' + clients + ' now connected.');
+//
+//   // listen to incoming bids
+//   var incomingBid = socket.on('bid', function(bidEvent) {
+//     console.log('Incoming bid: ' + bidEvent.bidAmount + ' from ' + bidEvent.userId + ' for lot ' + bidEvent.lotNumber); // submitted bid is transmitted back to server; use content to parse for bidValue, email, lotId to update db (pass to another route js???)
+//     var bidData = bidEvent;
+//     console.log('updating bids TABLE with: ' + bidData);
+//
+//     // SQL statement to update bid TABLE
+//     db.one('INSERT INTO bids VALUES (DEFAULT, $1, NOW(), FALSE, $2, $3) RETURNING *', [bidEvent.bidAmount, bidEvent.userId, bidEvent.lotNumber])
+//       .then(bidData => {
+//         console.log('bids TABLE was updated successfully as id#', bidData.id); // confirm bid data updated to db
+//       })
+//       .catch(error => {
+//         console.log('bids TABLE was not updated successfully! ', error);
+//       });
+//
+//   //when some client disconnects:
+//     socket.on('disconnect', function() {
+//       clients--; // client counter decreases
+//       console.log('A client has disconnected. ' + clients + ' still connected.');
+//     });
+//   });
+// });
 // var sendBidDataToServer = socketServer;
 // console.log(sendBidDataToServer);
 // console.log('bid data to send: ' + bidData);
@@ -102,6 +103,8 @@ app.get('/', (req, res, next) => {
     res.redirect('/login');
   }
 });
+
+
 app.listen(port, ()=> {
   console.log(`Server listening on port`)
 });
--- app/public/js/socketIOBid.js
+++ app/public/js/socketIOBid.js
@@ -1,10 +1,10 @@
-$(function () {
-  console.log('this works');
-  var socket = io.connect('http://localhost:8000/');
-  var bidSubmitted = $('#bid-amount').attr("bid-amount");
-  console.log(bidSubmitted);
-  var bidEvent = { bidAmount: bidSubmitted, userId: $('.navbar-text').attr("userId"), lotNumber: $('.lot-number').attr("lot-number") };
-  socket.emit('bid', bidEvent ); // bidEvent is the object sent by socket to the server
-  console.log(bidEvent);
-  return false;
-});
+// $(function () {
+//   console.log('this works');
+//   var socket = io.connect('http://localhost:8000/');
+//   var bidSubmitted = $('#bid-amount').attr("bid-amount");
+//   console.log(bidSubmitted);
+//   var bidEvent = { bidAmount: bidSubmitted, userId: $('.navbar-text').attr("userId"), lotNumber: $('.lot-number').attr("lot-number") };
+//   socket.emit('bid', bidEvent ); // bidEvent is the object sent by socket to the server
+//   console.log(bidEvent);
+//   return false;
+// });
