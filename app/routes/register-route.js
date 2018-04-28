8fc39c3b15ec3396f4fb947618ad93d046ecbe0f
Christian Lowe <christglowe@gmail.com>
2018-04-28 13:13:22 -0500

    changed register route to point towards auction page if the user is already logged in

--- app/routes/register-route.js
+++ app/routes/register-route.js
@@ -13,7 +13,7 @@ router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/register', (req, res) => {
   if (req.session.user) {
-    res.redirect('/artwork');
+    res.redirect('/auction');
   } else {
     res.render('layouts/register');
   }
@@ -32,18 +32,21 @@ router.post('/register', (req, res) => {
     })
   }
     try {
+
       //check if user has already been saved to database to prevent duplicate registrations
       db.one('SELECT * FROM users WHERE email = $1', [email])
       .then( ()=> {
         console.log('username already exists');
         res.send('username already exists')
       }).catch( err => {
-        //if user is not in database, hash their password and saved their data to database
+
+        //if user is not in database, hash their password and save their data to database
         bcrypt.hash(password, saltRounds)
         .then( (hash) => {
           hashedpwd = hash;
           db.any('INSERT INTO users(id, email, password, last_name, first_name, activation_hash) VALUES (DEFAULT, $1, $2, $3, $4, $5)', [email, hashedpwd, last_name, first_name, activationHash])
-          //log the user by storing their id in the session
+
+          //log the user in by storing their id in the session
           .then( data => {
           console.log('saved to database');
           req.session.user = {
@@ -53,7 +56,6 @@ router.post('/register', (req, res) => {
             lastName: data.last_name,
             verified: data.verified
           };
-
           res.redirect('/auction');
           sendMail(email);
         })
